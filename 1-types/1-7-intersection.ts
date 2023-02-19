f6901716b970ebdcaa7a38aa17ec16db31b22a30
yjleeinkr <lucy.lee930216@gmail.com>
2023-02-20 01:32:43 +0900

    study intersection type

---
+++ 1-types/1-7-intersection.ts
@@ -0,0 +1,25 @@
+{
+    // Intersection Type : And (&) 
+    type Student = {
+        name: string,
+        score: number
+    }
+
+    type Worker = {
+        employeeId: number,
+        work: () => void
+    }
+    
+    // person 인자 전달 시 Student와 Worker가 가져야할 속성을 다 포함해야한다.
+    function interWork(person: Student & Worker) {
+        console.log(person.name, person.employeeId, person.work())
+    }
+
+    // 아래 속성 중 하나라도 빠지면 에러
+    interWork({
+        name: 'yj',
+        score: 500,
+        employeeId: 3,
+        work: () => {}
+    })
+}
