eed066920423f29c53ada5bca5e1481f2908369f
yjleeinkr <lucy.lee930216@gmail.com>
2023-02-27 23:46:41 +0900

    study encapsulation, abstraction

---
+++ 3-oop/3-3-encapsulation.ts
@@ -0,0 +1,102 @@
+{
+  type Coffee = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  // Encapsulation - 은닉화
+  // public - 아무 키워드도 쓰지 않으면 public이 default라 외부에서 접근이 가능하다
+  // private - 외부에서 접근 불가하지만 클래스 내부에서만 접근 가능
+  // protected - 외부에선 접근할 수 없지만 해당 클래스를 상속한 자식 클래스 내부에선 접근 가능
+
+  class CoffeeMaker {
+    private static BEANS_GRAM_PER_SHOT: number = 7; // 외부에선 보이지 않고 직접적인 변경도 불가하다
+    private coffeeBeans: number = 0; // instance (object) level
+
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    makeCoffee(shots: number): Coffee {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    // static 함수
+    // 클래스 내부의 어떤 속성값도 필요하지 않기 때문에 static을 붙여준다.
+    // 어떤 오브젝트를 만들어주는 함수를 제공해준다면 누군가가 생성자를 이용해서 생성하는 것을 금지하기 위해 쓴다.
+    // 이 경우 contructor 에 private을 불여 외부에선 항상 static 함수를 사용하게끔 권장한다고 함
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    // private 설정으로 외부에서 접근 불가한 데이터를 변경시켜주는 함수는 public으로 만들었다.
+    // 유효성 검사도 할 수 있어서 안정성 있다.
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+  }
+
+  // const maker = new CoffeeMaker(32);
+  // maker.fillCoffeeBeans(100)
+  // maker.coffeeBeans = -34;
+  // CoffeeMaker { coffeeBeans: -34 } invalid!
+  // public 일때 외부에서 값을 바꿀 수 있기 때문에 위험하다
+
+  //   console.log(maker);
+
+  class User {
+    firstName: string;
+    lastName: string;
+    // fullName: string;
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      // this.fullName = `${this.firstName} ${this.lastName}`; // 대신 getter를 사용해준다.
+    }
+  }
+
+  const user = new User("Steve", "Jobs");
+  console.log(user);
+  // User { firstName: 'Steve', lastName: 'Jobs', fullName: 'Steve Jobs' }
+  console.log(user.fullName); // Steve Jobs
+  user.firstName = "yj";
+  console.log(user.fullName); // yj Jobs
+  // User { firstName: 'yj', lastName: 'Jobs', fullName: 'Steve Jobs' }
+  // firstName을 바꿨어도 firstName만 바뀌고 이미 fullName이 정의 시 지정되어버렸기 때문에 바뀌지 않는다.
+  // 이를 개선하기 위해 getter를 사용해준다
+
+  class User2 {
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+    private internalAge = 4;
+    get age(): number {
+      return this.internalAge;
+    }
+    set age(num: number) {
+      if (num < 0) {
+        // 유효성 검사를 넣을 수도 있다.
+      }
+      this.internalAge = num;
+    }
+    constructor(private firstName: string, private lastName: string) {}
+  }
+  const user1 = new User2("yj", "Lee");
+  console.log(user1); // User2 { firstName: 'yj', lastName: 'Lee', internalAge: 4 }
+  user1.age = 8;
+  console.log(user1); // User2 { firstName: 'yj', lastName: 'Lee', internalAge: 8 }
+}
---
+++ 3-oop/3-4-abstraction.ts
@@ -0,0 +1,124 @@
+{
+    type Coffee = {
+        shots: number;
+        hasMilk: boolean;
+    };
+
+    // 또는 interface를 통해서 추상화가 가능하다.
+    // 계약서와 같은 것
+
+    interface CoffeeMaker {
+        makeCoffee(shots: number): Coffee;
+    }
+    // 보통 interface 명 앞에 I 라는 prefix를 붙이는 사람들도 많지만, 인터페이스는 외부적으로 사용하는 이름이기 때문에 최대한 간단한 이름을 붙여주고, 구현하는 클래스에서 다른 이름을 가져가는 게 더 좋다.
+    
+    interface CommercialCoffeeMaker {
+        makeCoffee(shots: number): Coffee;
+        fillCoffeeBeans(beans: number): void;
+        clean(): void;
+    }
+
+    // 클래스 CoffeeMachine은 인터페이스 CoffeeMaker를 구현하는 클래스이다.
+    // 인터페이스가 지정된 클래스는 인터페이스에 규약된 모든 함수들을 구현해야한다.
+    class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
+        private static BEANS_GRAM_PER_SHOT: number = 7;
+        private coffeeBeans: number = 0;
+
+        private constructor(coffeeBeans: number) {
+            this.coffeeBeans = coffeeBeans;
+        }
+
+        private grindBeans(shots: number) {
+            console.log(`grinding beans for ${shots}`);
+            if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT) {
+                throw new Error("Not enough coffee beans!");
+            }
+            this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
+        }
+
+        private preheat(): void {
+            console.log("heating up...🔥");
+        }
+
+        private extract(shots: number): Coffee {
+            console.log(`Pulling ${shots} shots...☕️`);
+            return {
+                shots,
+                hasMilk: false,
+            };
+        }
+        makeCoffee(shots: number): Coffee {
+            this.grindBeans(shots);
+            this.preheat();
+            return this.extract(shots);
+        }
+
+        static makeMachine(coffeeBeans: number): CoffeeMachine {
+            return new CoffeeMachine(coffeeBeans);
+        }
+
+        fillCoffeeBeans(beans: number) {
+            if (beans < 0) {
+                throw new Error("value for beans should be greater than 0");
+            }
+            this.coffeeBeans += beans;
+        }
+        clean(): void {
+            console.log('cleaning the machine...🧼')
+        }
+    }
+
+    class AmateurUser{
+        constructor(private machine: CoffeeMaker) { }
+        makeCoffee() {
+            const coffee = this.machine.makeCoffee(2);
+            console.log(coffee)
+        }
+    }
+
+    class ProBarista{
+        constructor(private machine: CommercialCoffeeMaker) { }
+        makeCoffee() {
+            const coffee = this.machine.makeCoffee(2);
+            console.log(coffee);
+            this.machine.fillCoffeeBeans(45);
+            this.machine.clean();
+        }
+    }
+
+        const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+        maker.fillCoffeeBeans(32);
+        maker.makeCoffee(2);
+        // maker. 하면 많은 함수가 보인다. 사용자는 어떤 함수를 써야할지 헷갈릴 수 있다.
+        // 이 때 도움을 주는 게 추상화이다!
+        // 추상화? 인터페이스를 간단하게 만듦으로써 사용자가 간편하게 많은 생각을 하지 않고도 심플하게 사용할 수 있도록 도와준다.
+
+        // 접근제어자를 붙여서 즉 encapsulation을 통해 추상화가 가능하다.
+        // 노출될 필요가 없는 함수 앞에 접근제어자 private을 붙여준다.
+
+        // 또는 interface를 통해서 추상화가 가능하다.
+
+        const maker2: CoffeeMaker = CoffeeMachine.makeMachine(32);
+        // maker2.fillCoffeeBeans(32); 인터페이스 CoffeeMaker에는 fillCoffeeBeans가 정의되어있지 않아서 사용하지 못한다.
+        // => 인터페이스를 이용해 내가 얼마만큼의 행동을 약속, 허용할건 지 결정할 수 있다.
+        maker2.makeCoffee(2);
+
+        // class CoffeeMachine 타입 지정 시 : public으로 지정한 모든 함수에 접근 가능
+        // interface로 (CoffeeMaker나 CommercialCoffeeMaker) 타입을 제한해서 받게 되면 해당 인터페이스 내에 정의된 함수에만 접근 가능하다!
+
+        const maker3: CommercialCoffeeMaker = CoffeeMachine.makeMachine(32);
+        maker3.fillCoffeeBeans(32);
+        maker3.makeCoffee(3);
+        maker3.clean();
+    
+    console.log('----------')
+    const coffeeMaker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+    const amateur = new AmateurUser(coffeeMaker);
+    const pro = new ProBarista(coffeeMaker);
+    amateur.makeCoffee();
+    console.log("----------");
+    pro.makeCoffee()
+
+    // 📌 동일한 오브젝트 인스턴스일지라도 이 오브젝트는 2가지의 인터페이스를 구현하기 때문에 아마추어 유저와 프로 바리스타는 커피머신을 받아오는 게 아니라 각 인터페이스를 생성자에서 받아오므로 클래스보다는 좁은 범위의 인터페이스에서 규약된 함수들로만 접근 가능하다!!
+}
+
