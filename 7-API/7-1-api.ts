f72cf92a18ec4c3af3da566ee71983867f7b3699
yjleeinkr <lucy.lee930216@gmail.com>
2023-04-10 23:53:32 +0900

    make stack w/typescript

---
+++ 7-API/7-1-api.ts
@@ -0,0 +1,9 @@
+// Array
+
+type Student = {
+    passed: boolean;
+}
+
+const students: Student[] = [{ passed: true }, { passed: true }, { passed: false }];
+const result = students.every((student)=>student.passed)
+console.log(result)
