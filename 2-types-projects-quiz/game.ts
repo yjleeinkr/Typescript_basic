a777e2f51258b90691524a16aa70c7dbc0bddc45
yjleeinkr <lucy.lee930216@gmail.com>
2023-07-17 23:05:52 +0900

    prettier 적용됨

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,42 +1,43 @@
-{/**
- * Let's make a game 🕹
- */
+{
+  /**
+   * Let's make a game 🕹
+   */
 
-type Position = {
-    x: number,
-    y: number
-}
+  type Position = {
+    x: number;
+    y: number;
+  };
 
-let position: Position = { x: 0, y: 0 };
+  let position: Position = { x: 0, y: 0 };
 
-type Command = "up" | "down" | "left" | "right";
+  type Command = "up" | "down" | "left" | "right";
 
-function move(command: Command) {
+  function move(command: Command) {
     switch (command) {
-        case 'up':
-            position.y += 1;
-            break;
-        case 'down':
-            position.y -= 1;
-            break;
-        case 'left':
-            position.x -= 1;
-            break;
-        case 'right':
-            position.x += 1;
-            break;
-        default: 
-            throw new Error("unknown command!");
+      case "up":
+        position.y += 1;
+        break;
+      case "down":
+        position.y -= 1;
+        break;
+      case "left":
+        position.x -= 1;
+        break;
+      case "right":
+        position.x += 1;
+        break;
+      default:
+        throw new Error("unknown command!");
     }
-}
+  }
 
-console.log(position); // { x: 0, y: 0}
-move('up');
-console.log(position); // { x: 0, y: 1}
-move('down');
-console.log(position); // { x: 0, y: 0}
-move('left');
-console.log(position); // { x: -1, y: 0}
-move('right');
-console.log(position); // { x: 0, y: 0}
+  console.log(position); // { x: 0, y: 0}
+  move("up");
+  console.log(position); // { x: 0, y: 1}
+  move("down");
+  console.log(position); // { x: 0, y: 0}
+  move("left");
+  console.log(position); // { x: -1, y: 0}
+  move("right");
+  console.log(position); // { x: 0, y: 0}
 }
