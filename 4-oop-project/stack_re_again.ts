49ea11d47e0c2f64a9ad7cd728cdcb840f5bbdbc
yjleeinkr <lucy.lee930216@gmail.com>
2023-11-28 23:32:54 +0900

    stack (단일 연결 리스트) 객체지향적으로 구현해보기

---
+++ 4-oop-project/stack_re_again.ts
@@ -0,0 +1,51 @@
+namespace Stack {
+  interface Stack {
+    readonly size: number;
+    push(value: string): void;
+    pop(): string;
+  }
+
+  type StackNode = {
+    readonly value: string;
+    readonly next?: StackNode;
+  }
+
+  class StackImpl implements Stack {
+    private _size: number = 0;
+    private head?: StackNode;
+
+    constructor(private capacity: number) { }
+    
+    get size() {
+      return this._size;
+    }
+
+    push(value: string) {
+      if (this._size === this.capacity) throw new Error('Stack is full!');
+      const node: StackNode = { value, next: this.head };
+      this.head = node;
+      this._size++;
+    }
+
+    pop(): string {
+      if (this.head == null) throw new Error('Stack is empty!');
+      const node: StackNode = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
+    }
+  }
+
+  const stack = new StackImpl(10);
+  stack.push('1')
+  stack.push('2')
+  stack.push('3')
+  stack.push('4')
+  stack.push('5')
+  stack.push('5')
+  stack.push('5')
+  while (stack.size !== 0) {
+   console.log(stack.pop()) 
+  }
+
+}
