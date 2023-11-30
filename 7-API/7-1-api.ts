ee515f0c9a256145a50055f526e97f1c2ecc921d
yjleeinkr <lucy.lee930216@gmail.com>
2023-12-01 00:05:52 +0900

    type assertion - as, is

--- 7-API/7-1-api.ts
+++ 7-API/7-1-api.ts
@@ -7,3 +7,19 @@ type Student = {
 const students: Student[] = [{ passed: true }, { passed: true }, { passed: false }];
 const result = students.every((student)=>student.passed)
 console.log(result)
+
+class Animal { }
+class Cat extends Animal {
+    isCat: boolean = true;
+}
+
+class Dog extends Animal {
+    isDog: boolean = true;
+}
+
+const animals: Animal[] = [new Cat(), new Cat(), new Dog()];
+
+function isCat(animal: Animal): animal is Cat {
+    return (animal as Cat).isCat !== undefined;
+}
+console.log(animals.every(isCat));
