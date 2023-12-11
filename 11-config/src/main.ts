020288764e5fe31ece8386d235e5eec45b94f10b
yjleeinkr <lucy.lee930216@gmail.com>
2023-12-12 00:37:56 +0900

    tsconfig 옵션 파헤치기

---
+++ 11-config/src/main.ts
@@ -0,0 +1,7 @@
+console.log('여기도 출력')
+// compilerOptions 
+// compilerOptions > outDir : 빌드한 결과물이 저장되는 디렉토리 설정
+// compilerOptions > rootDir : typescript가 있어야할 루트 디렉토리 지정, 이 디렉토리 바깥에 ts파일이 존재하면 컴파일 시 에러가 발생한다.
+
+// exclude : 컴파일하지 않을 파일들을 배열로 열거 시 해당 파일들은 제외하고 컴파일한다.
+// include : 컴파일할 파일들을 배열로 열거하면 해당 파일들만 컴파일한다.
