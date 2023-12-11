89089bfcba348a3b6a7b98f7f5ad7d2e70a22d79
yjleeinkr <lucy.lee930216@gmail.com>
2023-12-12 00:47:55 +0900

    디버깅 - sourceMap

--- 11-config/build/main.js
+++ 11-config/build/main.js
@@ -1,7 +1,19 @@
-"use strict";
+'use strict';
 console.log('여기도 출력');
 // compilerOptions 
 // compilerOptions > outDir : 빌드한 결과물이 저장되는 디렉토리 설정
 // compilerOptions > rootDir : typescript가 있어야할 루트 디렉토리 지정, 이 디렉토리 바깥에 ts파일이 존재하면 컴파일 시 에러가 발생한다.
 // exclude : 컴파일하지 않을 파일들을 배열로 열거 시 해당 파일들은 제외하고 컴파일한다.
 // include : 컴파일할 파일들을 배열로 열거하면 해당 파일들만 컴파일한다.
+class Car {
+    constructor() {
+        this.engine = 0;
+    }
+    move() {
+        const engine = this.engine + 1;
+        console.log(engine);
+    }
+}
+const car = new Car();
+car.move();
+//# sourceMappingURL=main.js.map
--- 11-config/src/main.ts
+++ 11-config/src/main.ts
@@ -1,3 +1,4 @@
+'use strict';
 console.log('여기도 출력')
 // compilerOptions 
 // compilerOptions > outDir : 빌드한 결과물이 저장되는 디렉토리 설정
@@ -5,3 +6,14 @@ console.log('여기도 출력')
 
 // exclude : 컴파일하지 않을 파일들을 배열로 열거 시 해당 파일들은 제외하고 컴파일한다.
 // include : 컴파일할 파일들을 배열로 열거하면 해당 파일들만 컴파일한다.
+
+class Car {
+  engine = 0;
+  move() {
+    const engine = this.engine + 1;
+    console.log(engine);
+  }
+}
+
+const car = new Car();
+car.move();
